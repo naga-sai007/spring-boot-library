a2baa83ab11ea9e206072aed5df9a577b7af12cb
naga-sai007 <60782699+naga-sai007@users.noreply.github.com>
2023-08-13 18:42:00 +0530

    Update cross-origin

--- src/main/java/com/nagasai/springbootlibrary/controller/MessagesController.java
+++ src/main/java/com/nagasai/springbootlibrary/controller/MessagesController.java
@@ -7,7 +7,7 @@ import com.nagasai.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin
 @RestController
 @RequestMapping("/api/messages")
 public class MessagesController {
